d35a1e26a967011d8e74f2a4afdb0cbd55407b63
sjizzle <96852421+sjizzle@users.noreply.github.com>
2022-12-04 19:12:58 -0500

    Update db.js
    Fixed following a previous commit -

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,12 @@
+/* Error message - localhost/Survey - //0.0.0.0:27017 - works -sourced from stackoverflow 
+localhost doesn't connect - mongodb - cloud connects but, its faulty */
+
+
 module.exports = 
-{
-    "URI": "mongodb://0.0.0.0:27017/Survey"
+{   
+    /* Working link */
+   "URI": "mongodb://0.0.0.0:27017/Survey"
+
+   /* MongoDB - Cloud link connects but, authentication fails to login - */
+    //"URI":"mongodb+srv://6AnSLelQcEtS8kld:a5_ACi7fUivMUVG@cluster0.kzoojkk.mongodb.net/Survey"
 }
